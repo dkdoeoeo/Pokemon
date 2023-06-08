3f4c86cfc0ae47634a5fa862dda12369c5b6dc14
dkdoeoeo <timy3260@gmail.com>
2023-06-08 15:01:24 +0800

    新增載入圖片

--- Pokemon/mainwindow.cpp
+++ Pokemon/mainwindow.cpp
@@ -15,4 +15,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+void MainWindow::initIcon()
+{
+    iconBlastoise = new QPixmap("./images/Blastoise.png");
+    iconCharizard = new QPixmap("./images/Charizard.png");
+    iconVenusaur = new QPixmap("./images/Venusaur.png");
+    iconPotion = new QPixmap("./images/Potion.png");
+    iconSuperPotion = new QPixmap("./images/SuperPotion.png");
+    iconHyperPotion = new QPixmap("./images/HyperPotion.png");
+    iconMaxPotion = new QPixmap("./images/MaxPotion.png");
+    //iconBackground = new QPixmap("./images/Background.png");
+    this->setStyleSheet("QMainWindow {background-image: url(:./images/Background.png;}");
+}
--- Pokemon/mainwindow.h
+++ Pokemon/mainwindow.h
@@ -3,6 +3,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPalette>
 
 
 
@@ -19,8 +20,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void initIcon();//initialize all icon
+
 private:
     Ui::MainWindow *ui;
+    QPixmap *iconBlastoise;//Blastoise icon
+    QPixmap *iconCharizard;//Charizard icon
+    QPixmap *iconVenusaur;//Venusaur icon
+    QPixmap *iconPotion;//Potion icon
+    QPixmap *iconSuperPotion;//SuperPotion icon
+    QPixmap *iconHyperPotion;//HyperPotion icon
+    QPixmap *iconMaxPotion;//MaxPotion icon
+    QPixmap *iconBackground;//Background icon
 };
 
 #endif // MAINWINDOW_H
